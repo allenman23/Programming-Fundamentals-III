5beea648b5a524080e755baefb5a0497bb04fbd0
allenman23 <42852849+allenman23@users.noreply.github.com>
2019-04-22 16:11:01 -0500

    Update sortedlist.cpp

--- Lab5/sortedlist.cpp
+++ Lab5/sortedlist.cpp
@@ -1,8 +1,41 @@
 // Allen Lim
 
 #include<iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
 //#include<random>
 //#include<ctime>
+
+template<class ItemType>
+class Node
+{
+private:
+   ItemType        item; // A data item
+   std::shared_ptr<Node<ItemType>> next; // Pointer to next node
+   
+public:
+   Node() {   };
+   Node(const ItemType& anItem) {  item = anItem; };
+   Node(const ItemType& anItem, std::shared_ptr<Node<ItemType>> nextNodePtr) { 
+   	item = anItem;
+   	next = nextNodePtr;
+   };
+   void setItem(const ItemType& anItem) { item = anItem; };
+   void setNext(std::shared_ptr<Node<ItemType>> nextNodePtr) { next = nextNodePtr; };
+   ItemType getItem() const { return item; };
+   auto getNext() const { return next; };
+
+};
+
+class PrecondViolatedExcep : public std::logic_error
+{
+public:
+   PrecondViolatedExcep(const std::string& message = "")
+         : std::logic_error("Precondition Violated Exception: " + message)
+	{
+	}
+};
 
 template<class ItemType>
 class SortedListInterface
@@ -60,7 +93,33 @@ public:
 	virtual ~SortedListInterface() { }
 };
 
-
+template<class ItemType>
+class LinkedSortedList : public SortedListInterface<ItemType>
+{
+private:
+Node<ItemType>* headPtr; // Pointer to first node in the chain int itemCount; // Current count of list items
+// Locates the node that is before the node that should or does
+// contain the given entry.
+// @param anEntry The entry to find.
+// @return Either a pointer to the node before the node that contains
+// or should contain the given entry, or nullptr if no prior node exists. Node<ItemType>* getNodeBefore(const ItemType& anEntry) const;
+// Locates the node at a given position within the chain.
+Node<ItemType>* getNodeAt(int position) const;
+// Returns a pointer to a copy of the chain to which origChainPtr points.
+Node<ItemType>* copyChain(const Node<ItemType>* origChainPtr);
+public:
+LinkedSortedList();
+LinkedSortedList(const LinkedSortedList<ItemType>& aList);
+virtual ~LinkedSortedList();
+void insertSorted(const ItemType& newEntry); bool removeSorted(const ItemType& anEntry);
+int getPosition(const ItemType& newEntry) const;
+// The following methods are the same as given in ListInterface:
+bool isEmpty() const;
+int getLength() const;
+bool remove(int position);
+void clear();
+ItemType getEntry(int position)/* const throw(PrecondViolatedExcep)*/;
+};
 
 int main()
 {
